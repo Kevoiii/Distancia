62799a626e712e1b90449379fb135362fbaef415
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-10-14 07:51:28 -0600

    Archivo subido

---
+++ programa2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int x1, x2, y1, y2;
+cout<<"x1: ";
+cin>>x1;
+cout<<"y1: ";
+cin>>y1;
+cout<<"x2: ";
+cin>>x2;
+cout<<"y2: ";
+cin>>y2;
+float distancia= sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+cout<<"la distancia es: "<<distancia;
+
+
+
+
+
+
+
+
+}
